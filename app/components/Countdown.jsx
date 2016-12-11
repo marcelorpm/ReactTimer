459b0bf7aaf85a4cb4b1481fbf06a68d29d6e189
Marcelo <marcelo@Marcelos-Mac-mini.local>
2016-12-11 20:39:20 -0200

    Add static controls component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 var Countdown = React.createClass({
   getInitialState: function() {
@@ -38,6 +39,7 @@ var Countdown = React.createClass({
       <div>
         <Clock totalSeconds={count}/>
         <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+        //<Controls countdownStatus={'started'}/>
       </div>
     );
   }
